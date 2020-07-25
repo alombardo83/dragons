a792c307151e6a91f925dfd1106a515d67bc6fa0
rerou83 <aurelien.lombardo@gmail.com>
2020-07-26 00:16:49 +0200

    Ajout page profil + Amélioration menu

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('signup/', views.signup_view, name='signup'),
-    path('sent/', views.ActivationSentView.as_view(), name="activation_sent"),
+    path('sent/', views.ActivationSentView.as_view(), name='activation_sent'),
     path('activate/<slug:uidb64>/<slug:token>/', views.activate, name='activate'),
+    path('accounts/profile/', views.profile, name='profile'),
 ]
--- core/views.py
+++ core/views.py
@@ -11,7 +11,7 @@ from django.template.loader import render_to_string
 from django.views import generic
 from django.conf import settings
 
-from .forms import SignUpForm
+from .forms import SignUpForm, ProfileForm
 from .tokens import account_activation_token
 
 class HomePageView(generic.TemplateView):
@@ -19,6 +19,54 @@ class HomePageView(generic.TemplateView):
 
 class ActivationSentView(generic.TemplateView):
     template_name = 'registration/activation_sent.html'
+    
+def profile(request):
+    if not request.user.is_authenticated:
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+
+    user = request.user
+    data = {
+        'username': user.username,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'email': user.email,
+    }
+
+    if request.method  == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.username = form.cleaned_data.get('username')
+            user.profile.first_name = form.cleaned_data.get('first_name')
+            user.profile.last_name = form.cleaned_data.get('last_name')
+            user.profile.email = form.cleaned_data.get('email')
+            user.save()
+            
+            if form.has_changed() and 'email' in form.changed_data:
+                # user can't login until link confirmed
+                user.is_active = False
+                user.save()
+                
+                current_site = get_current_site(request)
+                subject = 'Vérifier votre nouveau courriel'
+                # load a template like get_template() 
+                # and calls its render() method immediately.
+                message = render_to_string('registration/activation_request.html', {
+                    'user': user,
+                    'protocol': settings.PROTOCOL,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    # method will generate a hash value with user related data
+                    'token': account_activation_token.make_token(user),
+                })
+                user.email_user(subject, message)
+                return redirect('activation_sent')
+        else:
+            print(form.errors)
+    else:
+        form = ProfileForm(data, instance=request.user)
+    return render(request, 'core/profile.html', {'form': form})
     
 def activate(request, uidb64, token):
     try:
@@ -42,7 +90,6 @@ def signup_view(request):
     if request.method  == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            print('OK')
             user = form.save()
             user.refresh_from_db()
             user.profile.first_name = form.cleaned_data.get('first_name')
