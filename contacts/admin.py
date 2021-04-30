67098247d4bf7eb2b3b5c966bf1245630ffce2f1
Aurélien LOMBARDO <aurelien.lombardo@gmail.com>
2021-04-30 22:54:08 +0200

    Mise en place object-tools pour génération convocations

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from datetime import date
 
 import nested_admin
+from django_object_actions import DjangoObjectActions
+from docxtpl import DocxTemplate
 
 from membership.admin import MemberInline
+from membership.models import Member
 from subscription.admin import ClientInline
 from .models import Person, Address
 
@@ -12,7 +16,7 @@ class AddressInline(nested_admin.NestedStackedInline):
 
 
 @admin.register(Person)
-class PersonAdmin(nested_admin.NestedModelAdmin):
+class PersonAdmin(DjangoObjectActions, nested_admin.NestedModelAdmin):
     list_display = ('last_name', 'first_name', 'birthdate', 'is_active')
     search_fields = ['last_name']
     inlines = [
@@ -27,3 +31,25 @@ class PersonAdmin(nested_admin.NestedModelAdmin):
     is_active.short_description = 'Adhésion active'
     is_active.admin_order_field = 'is_active'
     is_active.boolean = True
+
+    def generate_summons(self, request, queryset):
+        for m in Member.objects.all():
+            if m.is_active():
+                doc = DocxTemplate("summons_template.docx")
+                context = {
+                    'generate_date': date.today().strftime('%d %B %Y'),
+                    'lastname': m.person.last_name,
+                    'firstname': m.person.first_name,
+                    'address_line1': m.person.address.field1,
+                    'address_line2': m.person.address.field2,
+                    'address_line3': m.person.address.field3,
+                    'address_postal_code': m.person.address.postal_code,
+                    'address_city': m.person.address.city,
+                }
+                doc.render(context)
+                doc.save('summons_{}_{}.docx'.format(m.person.last_name, m.person.first_name))
+
+    generate_summons.label = 'Générer convocations'
+    generate_summons.short_description = 'Génère les convocations à l\'assemblée générale'
+
+    changelist_actions = ('generate_summons',)
