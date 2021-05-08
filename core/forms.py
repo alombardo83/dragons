0b0dd7da8747d54b1d74cc199367635602f49b55
Aurélien LOMBARDO <aurelien.lombardo@gmail.com>
2021-05-09 00:42:14 +0200

    Ajout recaptcha

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.core.exceptions import ValidationError
+
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=100, label='Prénom')
@@ -12,6 +12,7 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'newsletter_subscription')
+
 
 class ProfileForm(UserChangeForm):
     first_name = forms.CharField(max_length=100, label='Prénom')
