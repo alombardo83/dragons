1623425a59f9779cc7ce802ea86d800d7f93ba6f
Aurélien LOMBARDO <aurelien.lombardo@gmail.com>
2021-06-16 23:14:57 +0200

    Améliorations commande envoie d'emails

--- contacts_messages/management/commands/send_messages_queued.py
+++ contacts_messages/management/commands/send_messages_queued.py
@@ -23,40 +23,41 @@ class Command(BaseCommand):
         messages = Message.objects.filter(status=0).all()
         self.stdout.write(self.style.SUCCESS('Number of emails : %s' % len(messages)))
 
-        for message in messages:
-            message.status = 1
-            message.save()
-
-        with get_connection('message') as connection:
+        if len(messages) > 0:
             for message in messages:
-                m = render_to_string('contacts_messages/message.html', {
-                    'content': message.body
-                })
-
-                files = []
-                for file in message.attachments.all():
-                    f = file.attachment
-                    files.append((os.path.basename(f.name), f.read()))
-
-                total = 0
-                for i in range(0, len(emails_to), 50):
-                    emails = emails_to[i:i+50]
-                    total = total + len(emails)
-                    if total > 200:
-                        time.sleep(3700)
-                        total = len(emails)
-                    try:
-                        mail = EmailMultiAlternatives(subject=message.subject, from_email=connection.username,
-                                                      bcc=emails, connection=connection)
-
-                        mail.attach_alternative(m, 'text/html')
-                        for file in files:
-                            mail.attach(*file)
-                        mail.send()
-                    except Exception as err:
-                        self.stderr.write(self.style.ERROR(err))
-
-                message.status = 2
+                message.status = 1
                 message.save()
+
+            with get_connection('message') as connection:
+                for message in messages:
+                    m = render_to_string('contacts_messages/message.html', {
+                        'content': message.body
+                    })
+
+                    files = []
+                    for file in message.attachments.all():
+                        f = file.attachment
+                        files.append((os.path.basename(f.name), f.read()))
+
+                    total = 0
+                    for i in range(0, len(emails_to), 50):
+                        emails = emails_to[i:i+50]
+                        total = total + len(emails)
+                        if total > 200:
+                            time.sleep(3700)
+                            total = len(emails)
+                        try:
+                            mail = EmailMultiAlternatives(subject=message.subject, from_email=connection.username,
+                                                          bcc=emails, connection=connection)
+
+                            mail.attach_alternative(m, 'text/html')
+                            for file in files:
+                                mail.attach(*file)
+                            mail.send()
+                        except Exception as err:
+                            self.stderr.write(self.style.ERROR(err))
+
+                    message.status = 2
+                    message.save()
 
         self.stdout.write(self.style.SUCCESS('Finished'))
